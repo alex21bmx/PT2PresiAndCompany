bd4b14dce401ba06901e88ed82d5f4732d44fbb3
alex21bmx <73338062+alex21bmx@users.noreply.github.com>
2020-12-08 14:37:51 +0100

    Estructura Header

---
+++ extra/header.php
@@ -0,0 +1,20 @@
+<link rel="stylesheet" href="css/estils.css">
+<div id="header">
+    <div id="logoHeader">
+        <img id="logo" src="./src/logo.jpg" alt="">
+    </div>
+    <div id="titolHeader">
+        <h1>Viatges</h1>
+    </div>
+    <div id="botonsDivHeader">
+        
+            <button class="headerBttn"><img id="searchBttn" src="./src/search.png" alt=""></button>
+        
+        
+            <button class="headerBttn">Staff</button>
+        
+        
+            <button class="headerBttn">Log-in</button>
+        
+    </div>
+</div>
