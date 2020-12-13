adc40bab7def8f03d787ae633849784a37318269
a18arnferjer <73338058+a18arnferjer@users.noreply.github.com>
2020-12-13 14:54:15 +0100

    footerBertoTermnado

--- extra/footer.php
+++ extra/footer.php
@@ -2,25 +2,42 @@
 <div id="footer">
     <div id="upFooter">
         <div id="ptem">
-            <h4>PER TOT EL MÓN<h4>
+            <h4>ALL AROUND THE WORLD<h4>
+            <hr></hr>
         </div>
     </div>
     </div>
     <!--IZQUIERDA FOOTER-->
-    <div id="izquierdaFooter">
+    <div id="centroFooter">
+        <div id="ptem"> 
+                <div>
+                    <div id="otro">
+                        <h4>FOLLOW #PRESIBLOG</h4>
+                    </div>
+                        <button class="footerBttn" onclick=""><img id="socialBttn" src="./src/fb.png" alt=""></button>
+                
+                        <button class="footerBttn" onclick=""><img id="socialBttn" src="./src/instagram.png" alt=""></button>
+            
+                        <button class="footerBttn" onclick=""><img id="socialBttn" src="./src/twitter.png" alt=""></button>
+
+                        <button class="footerBttn" onclick=""><img id="socialBttn" src="./src/linkedIn.png" alt=""></button> 
+                </div> 
+        </div> 
+        <hr></hr>
+        <div>
+            <img id="berto" src="./src/berto.jpeg" alt="">
+        </div>
+    </div>
+
+    <div id="backToTop">
+        <div id="ptem">
+            <a id="backToTopBttn" href="#">BACK TO TOP <img src="./src/back.png" alt=""></a>            
+            <hr></hr>
+            <div id="cositas">
+                <h6>COPYRIGHT® 2020</h6>
+            </div>
+        </div>
+    </div>
     
-            <button class="footerBttn" onclick=""><img id="socialBttn" src="./src/fb.png" alt=""></button>
-        
-        
-            <button class="footerBttn" onclick=""><img id="socialBttn" src="./src/instagram.png" alt=""></button>
-        
-        
-            <button class="footerBttn" onclick=""><img id="socialBttn" src="./src/key.ico" alt=""></button>
-        
-    </div>
-    <!--DERECHA FOOTER-->
-    <div id="derechaFooter">
-        <p> que pasa aqui</p>
-    </div>
 </div>
 
