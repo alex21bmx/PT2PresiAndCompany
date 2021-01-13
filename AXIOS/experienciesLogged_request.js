898f6f9a11019f7358b034393679afe7e22979f6
alex21bmx <73338062+alex21bmx@users.noreply.github.com>
2021-01-13 10:55:55 +0100

    Detalles

--- AXIOS/experienciesLogged_request.js
+++ AXIOS/experienciesLogged_request.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     axios.get('./BD/api/api.php', {
-        timeout:4000,
+        timeout:6000,
         params: {
             size: document.getElementById("numero").value
             ,id: 7
@@ -21,7 +21,7 @@ window.onload = function() {
                         '<h4 class="localizacion">'+respuesta.data[index]["localizacion"]+'</h4>'+
                         '<h4 class="coordenadas">'+respuesta.data[index]["latitud"]+'-'+respuesta.data[index]["longitud"]+'</h4>'+ 
                     '</div>'+
-                    '<div class="desc">'+respuesta.data[index]["fecha_de_publicacion"]+' - '+respuesta.data[index]["id_usuario"]+'<div>'+
+                    '<div class="desc">'+respuesta.data[index]["fecha_de_publicacion"]+' - '+respuesta.data[index]["id_usuario"]+'</div>'+
                 '</div>';
             }
             localStorage.setItem('cadena',cadena);
